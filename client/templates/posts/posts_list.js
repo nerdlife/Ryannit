795b74e06943c5bfd19658423c24c8ad02a6e3f6
nerdlife <ryanuriewebdev@gmail.com>
2016-07-26 19:43:16 -0700

    created posts directory, basic templates, helper, and seeds

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,19 @@
+var postsData = [
+    {
+        title: 'Ryan Urie',
+        url: 'http://ryanurie.com'
+    },
+    {
+    title: 'Ryan Urie',
+    url: 'http://ryanurie.com'
+    }, 
+    {
+    title: 'Stack',
+    url: 'http://stackoverflow.com'
+  }
+];
+
+
+Template.postsList.helpers({
+    posts: postsData
+});
